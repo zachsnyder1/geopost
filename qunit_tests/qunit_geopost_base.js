bea2d9711daebfbf54588390fc4fad5e9983f0db
Zach Snyder <zachsnyder3@gmail.com>
2016-04-21 18:30:16 -0600

    adding geopost hint text

--- qunit_tests/qunit_geopost_base.js
+++ qunit_tests/qunit_geopost_base.js
@@ -2,7 +2,7 @@ QUnit.module('Test geopost_base.js functions');
 QUnit.test('test writeTrans', function(assert) {
 	var testfeat = new ol.Feature(new ol.geom.Point([0, 0]));
 	result = OL_OBJ.writeTrans([testfeat]);
-	expected = "<Transaction xmlns=\"http://www.opengis.net/wfs\" service=\"WFS\" version=\"1.1.0\" xsi:schemaLocation=\"http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><Insert><test_points xmlns=\"/mypoints\"><geometry><Point xmlns=\"http://www.opengis.net/gml\" srsName=\"EPSG:3857\"><pos>0 0</pos></Point></geometry></test_points></Insert></Transaction>";
+	expected = "<Transaction xmlns=\"http://www.opengis.net/wfs\" service=\"WFS\" version=\"1.1.0\" xsi:schemaLocation=\"http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><Insert><entries xmlns=\"/geopost\"><geometry><Point xmlns=\"http://www.opengis.net/gml\" srsName=\"EPSG:3857\"><pos>0 0</pos></Point></geometry></entries></Insert></Transaction>";
 	assert.equal(result, expected, 'WFS-t XML renders properly');
 });
 QUnit.test('test rescaleView', function(assert) {
