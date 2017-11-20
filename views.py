eb7722eec5d2fc8fd9d4f3663f174e9b618525ab
Zach Snyder <zachsnyder3@gmail.com>
2017-11-20 12:46:02 -0800

    change XSendfile dir to /tmp

--- views.py
+++ views.py
@@ -166,11 +166,11 @@ def vantechy(request):
     Download pdf of VanTechy presentation slideshow.
     """
     download_file = 'presentation.pdf'
-    download_path = '/home/zach/presentation.pdf'
+    download_path = '/tmp/{}'.format(download_file)
     file_len = str(os.stat(download_path).st_size)
     response = HttpResponse(content_type='application/force-download')
     response['Content-Disposition'] = 'attachment; filename="{}"' \
-                                       .format(smart_str(download_file))
+                                       .format(smart_str(download_file))              
     response['Content-Length'] = "{}".format(file_len)
     response['X_Sendfile'] = smart_str(download_path)
     return response
