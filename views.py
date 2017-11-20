4c5cb62952aadf069014bebca61b20822f767575
Zach Snyder <zachsnyder3@gmail.com>
2017-11-20 13:03:06 -0800

    removing quotations on XSendfile filename

--- views.py
+++ views.py
@@ -169,7 +169,7 @@ def vantechy(request):
     download_path = '/tmp/{}'.format(download_file)
     file_len = str(os.stat(download_path).st_size)
     response = HttpResponse(content_type='application/force-download')
-    response['Content-Disposition'] = 'attachment; filename="{}"' \
+    response['Content-Disposition'] = 'attachment; filename={}' \
                                        .format(smart_str(download_file))              
     response['Content-Length'] = "{}".format(file_len)
     response['X_Sendfile'] = smart_str(download_path)
